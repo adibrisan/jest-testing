5c4cdd10a7510f49a8b430f87cd9614a5f2bbf64
adibrisan <adi.brisan@yahoo.com>
2023-04-23 15:20:48 +0300

    Mocking basics

---
+++ spies_mocks/src/util/io.test.js
@@ -0,0 +1,24 @@
+import { it, expect, vi } from "vitest";
+import writeData from "./io";
+import { promises as fs } from "fs";
+
+vi.mock("fs");
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
+
+it("should execute the writeFile method", () => {
+  const testData = "Test";
+  const testFilename = "test.txt";
+
+  writeData(testData, testFilename);
+  //   expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+  //   expect(fs.writeFile).toBeCalled();
+  expect(fs.writeFile).toBeCalledWith(testFilename, testData);
+});
