dbe18e502a7982d98df86a271f82ea3332c1234c
adibrisan <adi.brisan@yahoo.com>
2023-04-29 19:55:02 +0300

    Local mock values

--- more_mocks/util/http.test.js
+++ more_mocks/util/http.test.js
@@ -1,10 +1,14 @@
 import { describe, it, expect, vi } from "vitest";
+import { HttpError } from "./errors";
 
 import { sendDataRequest } from "./http";
 
 const testResponseData = { testKey: "testssss" };
 const testFetch = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
+    if (typeof options.body !== "string") {
+      return reject("Not string.");
+    }
     const testResponse = {
       ok: true,
       json() {
@@ -24,4 +28,37 @@ it("should return any available response data", () => {
   const testData = { key: "test" };
 
   return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it("should convert the provided data to JSON before sending the request", async () => {
+  const testData = { key: "test" };
+  let errorMessage;
+
+  try {
+    await sendDataRequest(testData);
+  } catch (error) {
+    errorMessage = error;
+  }
+  expect(errorMessage).not.toBe("Not string.");
+});
+
+it("should throw an http error in case of non ok responses", () => {
+  testFetch.mockImplementationOnce((url, options) => {
+    //mock the implementation here to make ok to false
+    return new Promise((resolve, reject) => {
+      const testResponse = {
+        ok: false,
+        json() {
+          return new Promise((resolve, reject) => {
+            resolve(testResponseData);
+          });
+        },
+      };
+
+      resolve(testResponse);
+    });
+  });
+  const testData = { key: "test" };
+
+  return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError);
 });
